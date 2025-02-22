711fabac4b67ce2371f5f2d043bab072b78e3db6
Vaishnavi Nagane <vaishnavinagane986@gmail.com>
2025-02-22 13:22:05 +0530

    Create Missing Number.java

---
+++ Array/Missing Number.java
@@ -0,0 +1,22 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is 
+missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it 
+does not appear in nums.*/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int total=n*(n+1)/2;
+        int sum=0;
+
+        for(int i:nums){
+            sum+=i;
+        }
+        return total-sum;
+    }
+}
